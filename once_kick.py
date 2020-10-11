25b8f7629ec8a5f39c8430d1ee2424dfe7b75cd0
micchan814at15 <49190022+micchan814at15@users.noreply.github.com>
2020-10-11 10:09:53 +0900

    Create once_kick.py

---
+++ once_kick.py
@@ -0,0 +1,18 @@
+import discord
+import os
+import traceback
+from discord import member
+from discord.ext import commands
+
+client = discord.Client()
+token = os.environ['DISCORD_BOT_TOKEN']
+
+@client.event
+async def on_member_join(member):
+    if member.guild.id == (750342034124046397):
+        channel_sent = client.get_channel(750342034572967969)
+        if member.id == (514350156733218827):
+            user = discord.utils.get(member.guild.members, id=514350156733218827)
+            await user.kick()
+
+client.run(token)
